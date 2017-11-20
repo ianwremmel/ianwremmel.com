04f57d726a5d294a959b32e445e980e64cf6a072
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2017-11-19 20:54:05 -0500

    build(gatsby): add some plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-catch-links',
+    {
+      options: {
+        anonymize: true,
+        trackingId: ' UA-7509764-1'
+      },
+      resolve: 'gatsby-plugin-google-analytics'
+    },
+    'gatsby-plugin-react-next',
+    'gatsby-plugin-react-helmet',
+    // 'gatsby-plugin-netlify' should be last in the array
+    'gatsby-plugin-netlify'
   ],
   siteMetadata: {title: 'Gatsby Default Starter'}
 };
