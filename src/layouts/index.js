693b36fd02529bce3b83a9b000be74878b17378c
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2017-11-19 21:36:59 -0500

    feat(about): clean up gatsby defaults and add raw content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,39 +1,6 @@
-// eslint-disable-next-line import/no-unassigned-import
-import './index.css';
-
-import Link from 'gatsby-link';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Helmet from 'react-helmet';
-
-const Header = () =>
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem'
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem'
-      }}
-    >
-      <h1 style={{margin: 0}}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none'
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>;
-
 
 const TemplateWrapper = ({children}) =>
   <div>
@@ -50,17 +17,7 @@ const TemplateWrapper = ({children}) =>
         }
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0
-      }}
-    >
-      {children()}
-    </div>
+    {children()}
   </div>;
 
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,41 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import Link from 'gatsby-link';
+import Img from 'gatsby-image';
 
 const IndexPage = () =>
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>;
+  <main>
+    <h1>Ian W. Remmel</h1>
+    <h2>Software Engineer in San Francisco</h2>
+    <section>
+      <p>Afflicted with The Knack at an early age, I’ve always been a fixer and a builder. Growing up, I built things with snow, sand, dirt, or Legos (my parents preferred the Legos). In high school, I fixed PCs (wipe and reload, the Windows 9x cure-all). In grad school, I tried to fix Microsoft Outlook for home use (that’s a gross oversimplification - read my thesis or send me a message for more details); when market forces made that project impractical, I turned to web development.</p>
+      <p>Today, I write web apps to help teams - big and small - collaborate effectively. In April 2013, I joined Collaborate.com as Lead Web Developer. In December 2013, we were acquired by Cisco and I’ve become a software engineer in the Cloud Collaboration Technology Group.</p>
+    </section>
+    <section>
+      <section>
+        <h3>Work</h3>
+        <ul>
+          <li>Cisco</li>
+        </ul>
+      </section>
+
+      <section>
+        <h3>Education</h3>
+        <ul>
+          <li>Pennsylvania State University</li>
+        </ul>
+      </section>
+
+      <section>
+        <ul>
+          {/* TODO Contact Link */}
+          <li><a href="https://www.twitter.com/ianwremmel">Twitter</a></li>
+          <li><a href="https://github.com/ianwremmel">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/pub/ian-remmel/3/a32/208">LinkedIn</a></li>
+        </ul>
+      </section>
+
+    </section>
+  </main>;
 
 
 export default IndexPage;
