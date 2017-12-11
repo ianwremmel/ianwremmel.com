f7d463dbcb9a951cd4d564ef3a19b2b0f930a3cc
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2017-12-10 17:33:33 -0800

    feat(projects): add greenkeeper page

--- gatsby-node/index.js
+++ gatsby-node/index.js
@@ -1,7 +1,17 @@
+const {createRepoNode, listRepos} = require('./github');
+
 exports.onCreatePage = ({page, boundActionCreators: {createPage}}) => {
   if (page.path === '/') {
     page.layout = 'about';
     return createPage(page);
   }
   return Promise.resolve();
+};
+
+exports.sourceNodes = async({boundActionCreators: {createNode}}) => {
+  const repos = await listRepos();
+
+  for (const repo of repos) {
+    await createRepoNode(createNode, repo);
+  }
 };
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,10 +6,10 @@ const TemplateWrapper = ({children}) =>
   <div>
     <Helmet>
       <title>Ian W. Remmel | About</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossOrigin="anonymous"/>
     </Helmet>
     {children()}
   </div>;
-
 
 TemplateWrapper.propTypes = {children: PropTypes.func};
 
