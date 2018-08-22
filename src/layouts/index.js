31d0b83b8c4f9fb5e1940403893610afe6c2310a
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-08-22 15:45:35 +0200

    feat(about): improve formatting, add social links, update work

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,14 +2,20 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Helmet from 'react-helmet';
 
-const TemplateWrapper = ({children}) =>
+const TemplateWrapper = ({children}) => (
   <div>
     <Helmet>
       <title>Ian W. Remmel | About</title>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossOrigin="anonymous"/>
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossOrigin="anonymous"
+      />
     </Helmet>
     {children()}
-  </div>;
+  </div>
+);
 
 TemplateWrapper.propTypes = {children: PropTypes.func};
 
