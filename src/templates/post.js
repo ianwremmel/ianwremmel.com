d14f67b2f3b7741600b13cf1f1914a1e1cd692cc
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2019-02-04 19:27:25 -0800

    feat: add blog support

---
+++ src/templates/post.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Link, graphql} from 'gatsby';
+
+import Layout from '../components/layout';
+
+class PostTemplate extends React.Component {
+  render() {
+    const post = this.props.data.markdownRemark;
+    const siteTitle = this.props.data.site.siteMetadata.title;
+    const {previous, next} = this.props.pageContext;
+
+    return (
+      <Layout title={siteTitle}>
+        <h1>{post.frontmatter.title}</h1>
+        <p>{post.frontmatter.date}</p>
+        <div dangerouslySetInnerHTML={{__html: post.html}} />
+
+        <ul>
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </Layout>
+    );
+  }
+}
+
+export default PostTemplate;
+
+PostTemplate.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.string.isRequired,
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        title: PropTypes.string.isRequired
+      })
+    })
+  }),
+  pageContext: PropTypes.shape({
+    next: PropTypes.string,
+    previous: PropTypes.string
+  })
+};
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: {slug: {eq: $slug}}) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
+  }
+`;
