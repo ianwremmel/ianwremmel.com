064df642396f8226f55153a2f8320220677404fb
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2022-05-26 09:43:37 -0700

    refactor(landing-age): switch links to OutboundLink

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import {OutboundLink} from '../components/atoms/outbound-link';
 import {SocialLinks} from '../components/organisms/social-links';
 
 export default function Index() {
@@ -14,22 +15,20 @@ export default function Index() {
           <p>
             If you landed here, you probably met me at a conference or a meetup.
             Maybe I was talking about{' '}
-            <a
+            <OutboundLink
               href="https://www.linkedin.com/in/ianwremmel/"
-              rel="noopener noreferrer"
-              target="_blank"
+              title="My LinkedIn profile, which describes my current employment status"
             >
               what I do at work
-            </a>
+            </OutboundLink>
             , or I was excited about something I&apos;m doing with one of my
             side projects (probably{' '}
-            <a
+            <OutboundLink
               href="https://www.check-run-reporter.com/?utm_source=https%3A%2F%2Fwww.ianwremmel.com&utm_medium=website&utm_campaign=none"
-              rel="noopener noreferrer"
-              target="_blank"
+              title="Check Run Reporter helps you fix your CI failures faster by reducing the need to dig through poorly formatted logs."
             >
               check-run-reporter.com
-            </a>
+            </OutboundLink>
             ).
           </p>
         </div>
