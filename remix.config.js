4cb209b32b032195b966f0680412f5c72eb58eaf
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2022-08-06 22:41:59 -0700

    fix: switch to remark to get code blocks

--- remix.config.js
+++ remix.config.js
@@ -6,9 +6,18 @@
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
   server: './server.js',
-  serverBuildTarget: 'netlify'
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: ".netlify/functions-internal/server.js",
-  // publicPath: "/build/",
+  serverBuildTarget: 'netlify',
+  serverDependenciesToBundle: [
+    // This is suppsed to work, but the remix folks haven't quite gotten it
+    // working yet...
+    // ...getDependenciesToBundle(
+    //   'rehype-stringify',
+    //   'remark-gfm',
+    //   'remark-parse',
+    //   'remark-rehype',
+    //   'unified'
+    // )
+    // ...so we'll just bundle everything.
+    /./
+  ]
 };
