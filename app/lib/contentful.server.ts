6aebfed8d407273f41672a5fe3319b28af97fadd
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2022-08-15 21:25:04 -0700

    fix(blog): sort blog posts with newest first

--- app/lib/contentful.server.ts
+++ app/lib/contentful.server.ts
@@ -26,7 +26,7 @@ const IndexPagePost = gql`
 
 const ListPostsQuery = gql`
   query ListPostsQuery {
-    postCollection(limit: 10, order: sys_firstPublishedAt_ASC) {
+    postCollection(limit: 10, order: sys_firstPublishedAt_DESC) {
       items {
         ...IndexPagePost
       }
