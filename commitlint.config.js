2475bd44102866c45f659e4017086fa79c0cfae1
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2023-10-24 08:12:55 -0700

    chore: adopt ESM

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,10 @@
-'use strict';
-
 const IS_DBOT = (
   process.env.GITHUB_REF ||
   process.env.BUILDKITE_BRANCH ||
   ''
 ).includes('dependabot');
 
-module.exports = {
+export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [2, 'always', IS_DBOT ? Infinity : 100],
